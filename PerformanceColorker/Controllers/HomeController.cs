748a25c97658bf50ab066a0805894e9d6c88cf94
jvquiles <jvquiles@gmail.com>
2019-10-27 21:51:04 +0100

    Initial frontend Performance checker

---
+++ PerformanceColorker/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading;
+using System.Web.Mvc;
+using System.Web.SessionState;
+
+namespace PerformanceColorker.Controllers
+{
+    [SessionState(SessionStateBehavior.Disabled)]
+    public class HomeController : Controller
+    {
+        private static readonly DateTime UnixEpoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        
+        [HttpGet]
+        public double GetValue(string guid)
+        {
+            int milliseconds = 1000;
+            Thread.Sleep(milliseconds);
+
+            return (DateTime.UtcNow - UnixEpoch).TotalMilliseconds;
+        }
+        
+        [HttpPost]
+        public double PostValue(string guid)
+        {
+            Random random = new Random();
+            int milliseconds = random.Next(500, 1500);
+            Thread.Sleep(milliseconds);
+
+            return (DateTime.UtcNow - UnixEpoch).TotalMilliseconds;
+        }
+    }
+}
